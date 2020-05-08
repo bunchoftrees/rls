327eca71d4ebe4b6374334d746711e1e2fef1cb1
Forrest Hartley <forrest@forrestbear.org>
2020-05-08 02:47:31 -0400

    allow for user input

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,8 +1,22 @@
+use std::io::{self, Write};
+
 fn main() {
-    let name = format!("world");
-    greeter(name);
+    greeter("World".to_string());
+    greeter(get_name());
 }
 
 fn greeter(name: String) {
     println!("Hello, {}!", name);
+    io::stdout().flush().unwrap();
+}
+
+fn get_name() -> String {
+    print!("Please input your name:  ");
+    io::stdout().flush().unwrap();
+    let mut name = String::new();
+    io::stdin()
+            .read_line(&mut name)
+            .expect("Failed to read line");
+    let name = name.trim();
+    return name.to_string();
 }
